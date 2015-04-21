9fa28ca31552b48a87d5ba839f5825b8218114f9
kyolun <kyolun@gmail.com>
2015-04-21 12:02:53 +0800

    First

---
+++ MVC5_1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC5_1.Startup))]
+namespace MVC5_1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
